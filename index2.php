e7c1efa99fde970cc5ab723c3850cbde4118aadc
hafiz zubair <hafizzubair381@gmail.com>
2018-11-28 11:47:25 +0500

    index2.php from u2

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> index2</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+	<?php echo "i am new file index2.php from E:\xampp\htdocs\testing" ;?>
+
+
+</body>
+</html>
