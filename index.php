1bbc4b212891c5fed08185724604bb03ef213001
hafiz zubair <hafizzubair381@gmail.com>
2018-11-28 10:02:28 +0500

    index.php and style.css files are added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> sdfds</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<?php echo "i am php syntax" ;?>
+<h2 id="z">I am zubair</h2>
+<p>I am normal</p>
+<p style="color:red;">I am red</p>
+<p style="color:blue;">I am blue</p>
+<p style="font-size:50px;">I am big</p>
+
+</body>
+</html>
